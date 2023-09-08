34bd696aa6dce9c3bb5239beb969c2b747e4a9bd
孙阔 <1369926661@qq.com>
2023-09-08 22:32:46 +0800

    配置代码生成器模板

--- mall-generator/src/main/java/io/renren/MallGeneratorApplication.java
+++ mall-generator/src/main/java/io/renren/MallGeneratorApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+/**
+ * @author sunkuo
+ * @date 2023/9/8 21:53
+ */
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @MapperScan("io.renren.dao")
 public class MallGeneratorApplication {
