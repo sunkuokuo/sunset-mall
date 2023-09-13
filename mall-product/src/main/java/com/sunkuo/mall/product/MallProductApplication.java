b953fc05eba21721e8deeef90c5237f214637a91
孙阔 <1369926661@qq.com>
2023-09-13 13:36:33 +0800

    新增common模块

---
+++ mall-product/src/main/java/com/sunkuo/mall/product/MallProductApplication.java
@@ -0,0 +1,15 @@
+package com.sunkuo.mall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sunkuo
+ * @date 2023/9/13 13:31
+ */
+@SpringBootApplication
+public class MallProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallProductApplication.class, args);
+    }
+}
